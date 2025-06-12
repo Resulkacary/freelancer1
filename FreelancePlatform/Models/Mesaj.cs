e1a7d325e207f639c1a119f90350e8bbcadc8514
Resul Kaçar <resulkacarystudio@gmail.com>
2025-06-12 13:16:58 +0300

    fix: .NET 8 null hataları çözüldü

--- FreelancePlatform/Models/Mesaj.cs
+++ FreelancePlatform/Models/Mesaj.cs
@@ -4,12 +4,12 @@
     {
         public int MesajID { get; set; }
         public int ProjeID { get; set; }
-        public string GonderenEmail { get; set; }
-        public string AliciEmail { get; set; }
-        public string GonderenAdSoyad { get; set; }
-        public string MesajIcerik { get; set; }
+
+        public string GonderenEmail { get; set; } = string.Empty;
+        public string AliciEmail { get; set; } = string.Empty;
+        public string GonderenAdSoyad { get; set; } = string.Empty;
+        public string MesajIcerik { get; set; } = string.Empty;
+
         public DateTime GonderimTarihi { get; set; }
     }
-
-
 }
